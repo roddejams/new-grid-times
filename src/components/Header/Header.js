0db994e8bb49360b64de44ce1a952364027358f0
James Rodden <james.rodden@savanta.com>
2022-01-20 16:53:46 +0000

    Add desktop header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { Menu, Search, User } from 'react-feather';
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES } from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
@@ -10,30 +10,86 @@ import Button from '../Button';
 
 const Header = () => {
   return (
-    <header>
-      <SuperHeader>
-        <Row>
-          <ActionGroup>
-            <button>
-              <Search size={24} />
-            </button>
-            <button>
-              <Menu size={24} />
-            </button>
-          </ActionGroup>
-          <ActionGroup>
-            <button>
-              <User size={24} />
-            </button>
-          </ActionGroup>
-        </Row>
-      </SuperHeader>
-      <MainHeader>
-        <Logo />
-      </MainHeader>
-    </header>
+    <>
+      <MobileHeader>
+        <SuperHeader>
+          <Row>
+            <ActionGroup>
+              <button>
+                <Search size={24} />
+              </button>
+              <button>
+                <Menu size={24} />
+              </button>
+            </ActionGroup>
+            <ActionGroup>
+              <button>
+                <User size={24} />
+              </button>
+            </ActionGroup>
+          </Row>
+        </SuperHeader>
+        <MainHeader>
+          <Logo />
+        </MainHeader>
+      </MobileHeader>
+      <DesktopHeader>
+        <MaxWidthWrapper>
+          <Row>
+            <ActionGroup>
+              <button>
+                <Search size={24} />
+              </button>
+              <button>
+                <Menu size={24} />
+              </button>
+            </ActionGroup>
+            <Logo />
+            <SubscriberActions>
+              <Button>
+                Subscribe
+              </Button>
+              <SubscriberText>
+                Already a subscriber?
+              </SubscriberText>
+            </SubscriberActions>
+          </Row>
+        </MaxWidthWrapper>
+      </DesktopHeader>
+    </>
   );
 };
+
+const MobileHeader = styled.header`
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`;
+
+const DesktopHeader = styled.header`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: block;
+    margin-top: 32px;
+    margin-bottom: 48px;
+  }
+`;
+
+const SubscriberActions = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+  align-self: flex-end;
+`;
+
+const SubscriberText = styled.a`
+  font-style: italic;
+  text-decoration: underline;
+  font-size: 0.875rem;
+  color: ${COLORS.gray[900]}
+`;
 
 const SuperHeader = styled.div`
   padding: 16px 0;
@@ -44,6 +100,7 @@ const SuperHeader = styled.div`
 const Row = styled(MaxWidthWrapper)`
   display: flex;
   justify-content: space-between;
+  align-items: baseline;
 `;
 
 const ActionGroup = styled.div`
